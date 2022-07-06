86e62d1422e4b73c757bd59bf5eab3250409daad
Alexey Ospishchev <alexeyosp@gmail.com>
2022-07-06 21:14:11 +0300

    работает удаление и рендерится таблица

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,50 +1,68 @@
 import React, {useState} from "react";
 import API from "../api";
 
-const Users = () => {
-        
+const Users = () => {    
     
+    const [users, setCountPeople] = useState(() => API.users.fetchAll());
     
-    const [count, setCountPeople] = useState(() => API.users.fetchAll());
-    
-    const formCount = () => {
-        return count === 0 ? "Никто с тобой не тусанет!" : `${count} человек тусанет с тобой сегодня`;
+    const getCountMessage = () => {
+
+        let message = '';
+        message = users.length > 4 || users.length === 1 ? users.length + " человек тусанёт с тобой сегодня" :
+        users.length === 0 ? "Никто с тобой не тусанёт" :
+        users.length + " человека тусанёт с тобой сегодня";
+        return message;
     }
 
-    const handleDeleteUser =()=>{
-      
+    const handleDeleteUser = (id)=>{
+      console.log(id)
+      setCountPeople((prevState) => prevState.filter((user) => user._id !== id));
+    };
+
+const usersTableHeaderRender = () => {
+      return (
+        users.length !== 0
+        && <tr>
+          <th scope="col">Имя</th>
+          <th scope="col">Качества</th>
+          <th scope="col">Профессия</th>
+          <th scope="col">Встретился, раз</th>
+          <th scope="col">Оценка</th>
+          <th scope="col"></th>
+        </tr>
+      )
     }
 
+    const usersTableBodyRender = () => {
+      return (
+        users.length !== 0 && users.map((user) => (
+      <tr key={user._id}>
+        <td scope="col">{user.name}</td>
+        <td scope="col">{user.qualities.map((qualitie) => <span className={"badge m-1 bg-" + qualitie.color} key={qualitie._id}>{qualitie.name}</span>)}</td>
+        <td scope="col" key={user.profession._id}>{user.profession.name}</td>
+        <td scope="col">{user.completedMeetings}</td>
+        <td scope="col">{user.rate}</td>
+        <td scope="col"><button type="button" className="btn btn-primary" onClick={() => handleDeleteUser(user._id)}>Удалить</button></td>
+      </tr>
+        ))
+      )
+    }
+
+    
     
     return(    
     <>
-    <p>{formCount()} </p>
-    
-    <table className="table-primary">
-    <thead>
-      <tr>
-        <th scope="col">Имя</th>
-        <th scope="col">Качества</th>
-        <th scope="col">Профессия</th>
-        <th scope="col">Встретился, раз</th>
-        <th scope="col">Оценка</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        
-        <td>Джон Дориан</td>
-        <td><span class="badge bg-primary">Нудила</span><span class="badge bg-primary m-2">Неуверенный</span><span class="badge bg-primary">Странный</span> </td>
-        <td>Доктор</td>
-        <td>36</td>
-        <td>2.5/5</td>
-        <td><button className="btn btn-primary" type="button" onClick={handleDeleteUser}>Удалить</button></td>
-      </tr>
+      <h1>{getCountMessage()}</h1>
       
-    </tbody>
-  </table>
-    </>)
-    
+      <table className="table-primary">
+        <thead>
+          {usersTableHeaderRender()}
+        </thead>
+        <tbody>
+          {usersTableBodyRender()}
+        </tbody>
+    </table>
+    </>)    
 };
 
 export default Users;
