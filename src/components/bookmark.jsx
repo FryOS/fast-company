6ff645bf0b328dcafaa0e3c0a482097bf66906d5
Alexey Ospishchev <alexeyosp@gmail.com>
2022-07-15 19:42:11 +0300

    Подправил отображение иконки Избранное

--- src/components/bookmark.jsx
+++ src/components/bookmark.jsx
@@ -2,8 +2,9 @@ import React from "react";
 
 const Bookmark = ({status, ...rest}) => {
     return (
-        <button className="table-primary" {...rest} >
-            <i className={status ? "bi bi-bookmark" : "bi bi-bookmark-fill"}></i>
+        <button {...rest} >
+            {console.log(status)}
+            <i className={status ? "bi bi-bookmark-fill" : "bi bi-bookmark"}></i>
         </button>
     )
 }
