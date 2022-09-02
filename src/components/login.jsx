ad357587ae39e3c089c0b2a30a6a22b5d312de0c
Ospishchev Alexey <alexeyosp@gmail.com>
2022-09-02 11:31:38 +0300

    сделали логин и взяли для него состояние

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
-    return <h1>Login</h1>;
+    const [email, setEmail] = useState();
+    // const [password, setPassword] = useState(); // Для каждого поля делать свое состояние. Что не есть хорошо
+
+    const handleChange = (event) => {
+        setEmail(event.target.value);
+        console.log(event.target.value);
+    };
+
+    return (
+        <form action="">
+            <div>
+                <label htmlFor="email">Email</label>{" "}
+                <input
+                    type="text"
+                    id="email"
+                    value={email}
+                    onChange={handleChange}
+                />
+            </div>
+            <div>
+                <label htmlFor="password">Password</label>{" "}
+                <input type="password" id="password" />
+            </div>
+        </form>
+    );
 };
 
 export default Login;
